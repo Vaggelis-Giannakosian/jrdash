8c210dfe5e2544dff293ccf4a537b37051e7e144
Vagelis Giannakosian <e.giannakosian@gmail.com>
2019-06-11 21:43:37 +0300

    created Home controller - added simple login form to home view - created inc folder in views and added header and footer views

---
+++ application/views/home/home_view.php
@@ -0,0 +1,35 @@
+
+
+
+<div class="row">
+
+<div class="span6">
+
+
+
+<form action="" class="form-horizontal" method="post">
+
+    <div class="control-group">
+        <label for="" class="control-label">Login</label>
+        <div class="controls">
+            <input class="input-xlarge" type="text" name="login">
+        </div>
+    </div>
+
+    <div class="control-group">
+        <label for="" class="control-label">Password</label>
+        <div class="controls">
+            <input class="input-xlarge" type="password" name="password">
+        </div>
+    </div>
+
+    <div class="control-group">
+        <div class="controls">
+            <input class="btn btn-primary" type="submit" value="Login" name="password">
+        </div>
+    </div>
+
+</form>
+
+</div>
+</div>
---
+++ application/views/inc/header_view.php
@@ -0,0 +1,25 @@
+<?php
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>CodeIgniter Ftw</title>
+    <link rel="stylesheet" href="<?= base_url() ?>public/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?= base_url() ?>public/css/style.css">
+
+    <script src="<?= base_url() ?>public/js/jquery.js"></script>
+    <script src="<?= base_url() ?>public/js/bootstrap.js"></script>
+</head>
+<body>
+
+<header>
+    Jr Dashboard
+</header>
+<!--start wrapper-->
+<div class="wrapper">
+
